02be6bad4e257e4279ab08d6303f263c3978ba8b
HyunseoY <maricat0715@gmail.com>
2023-05-27 20:50:14 +0900

    start

---
+++ index.js
@@ -0,0 +1,16 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTY5OThlOWQ3MWIzMGMyNjA5NGIzMjAzMTU5YWRlMSIsInN1YiI6IjY0NzA4OTAxNzI2ZmIxMDBlMWMzMTQ3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9aRWYGmmSO_w-LR_sSCNqaiyee3BBj_703_ymo46wzc',
+  },
+};
+
+fetch(
+  'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
