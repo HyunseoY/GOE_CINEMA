c0217c845b321893e070aa70cc166cb3c0d5b068
HyunseoY <maricat0715@gmail.com>
2023-05-29 22:10:10 +0900

    js로 인기영화 데이터 불러오고 html문서를 분할 한 뒤 a로 링크

--- index.js
+++ index.js
@@ -7,10 +7,41 @@ const options = {
   },
 };
 
+//인기
+fetch('https://api.themoviedb.org/3/movie/popular?language=ko&page=1', options)
+  .then((response) => response.json())
+  .then((data) => {
+    const moviesContainer = document.getElementById('movies');
+
+    // 데이터를 HTML로 변환하여 동적으로 생성
+    data.results.forEach((movie) => {
+      const movieElement = document.createElement('div');
+      movieElement.textContent = movie.title;
+      moviesContainer.appendChild(movieElement);
+    });
+  })
+  .catch((err) => console.error(err));
+
+//현재상영
 fetch(
-  'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+  'https://api.themoviedb.org/3/movie/now_playing?language=ko&page=2',
   options
 )
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+// 높은평점
+fetch(
+  'https://api.themoviedb.org/3/movie/top_rated?language=ko&page=3',
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+// 개봉예정
+fetch('https://api.themoviedb.org/3/movie/upcoming?language=ko&page=4', options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
