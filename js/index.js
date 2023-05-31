36551633b2e82dc2aeffd61ce9c8a2106a71ec7b
HyunseoY <maricat0715@gmail.com>
2023-06-01 08:02:01 +0900

    자동 슬라이드 완성, 검색로직에서 불필요한 메소드, 함수 로직 제거

--- js/index.js
+++ js/index.js
@@ -21,6 +21,29 @@ fetch('https://api.themoviedb.org/3/movie/popular?language=ko&page=1', options)
       const card = createMovieCard(movie);
       moviesContainer.appendChild(card);
     });
+    // 초기 카드 배치
+    let cards = document.querySelector('.cards');
+    let movie = document.querySelectorAll('.movie'); // NodeList를 배열로 변환
+    let slideCount = movie.length;
+
+    makeClone();
+
+    function makeClone() {
+      // 뒤에 복제
+      for (let i = 0; i < slideCount; i++) {
+        let cloneSlide =
+          // a.cloneNode() a요소를 그대로 복사
+          // a.cloneNode(true) a요소에 자식요소까지 모두 복사
+          movie[i].cloneNode(true);
+        cloneSlide.classList.add('clone');
+        cards.appendChild(cloneSlide);
+      }
+
+      updateWidth();
+      setTimeout(function () {
+        cards.classList.add('animated');
+      }, 100);
+    }
   })
   .catch((err) => console.error(err));
 
@@ -28,7 +51,7 @@ function createMovieCard(movie) {
   // 카드 요소 생성
   const card = document.createElement('div');
   card.classList.add('movie');
-  card.onclick = () => showMovieDetail(movie.id);
+  cards.onclick = () => showMovieDetail(movie.id);
 
   // 포스터 이미지
   const poster = document.createElement('img');
@@ -39,42 +62,46 @@ function createMovieCard(movie) {
   return card;
 }
 
+// 클릭 시 인기영화 페이지로 이동
 function showMovieDetail() {
   const url = (href = '../html/popular.html');
   window.location.href = url;
 }
 
+// 여기부터 슬라이드 관련 함수들
 let cards = document.querySelector('.cards');
-let movie = Array.from(document.querySelectorAll('.movie')); // NodeList를 배열로 변환
+let movie = document.querySelectorAll('.movie');
 let currentIdx = 0;
 let slideCount = movie.length;
-let slideWidth = 247;
+let slideWidth = 246.66;
 let slideMargin = 20;
 
-// 초기 카드 배치
-for (let i = 0; i < slideCount; i++) {
-  cards.appendChild(movie[i].cloneNode(true));
+// 복제된 새로운 배열의 길이를 구하는 함수
+function updateWidth() {
+  let currentSlides = document.querySelectorAll('.movie');
+  let newSlideCount = currentSlides.length;
+
+  let newWidth =
+    (slideWidth + slideMargin) * newSlideCount - slideMargin + 'px';
+  cards.style.width = newWidth;
 }
 
-// 슬라이드 너비 설정
-cards.style.width =
-  (slideWidth + slideMargin) * slideCount - slideMargin + 'px';
-
+// 슬라이드 이동 함수
 function moveSlide(num) {
+  // cards.style.transition = 'left 1s ease-in-out';
   cards.style.left = -num * (slideWidth + slideMargin) + 'px';
   currentIdx = num;
 
   // 슬라이드가 끝에 도달하면 다시 처음으로 이동
-  if (currentIdx === slideCount) {
+  if (currentIdx == 20) {
     setTimeout(function () {
-      cards.style.transition = 'none';
+      cards.classList.remove('animated');
       cards.style.left = '0px';
       currentIdx = 0;
-    }, 500);
-
+    }, 1000);
     setTimeout(function () {
-      cards.style.transition = '';
-    }, 600);
+      cards.classList.add('animated');
+    }, 1050);
   }
 }
 
@@ -95,10 +122,10 @@ function stopSlide() {
   timer = undefined;
 }
 
-cards.addEventListener('mouseenter', function () {
+cards.addEventListener('mouseover', function () {
   stopSlide();
 });
 
-cards.addEventListener('mouseleave', function () {
+cards.addEventListener('mouseout', function () {
   autoSlide();
 });
--- js/now_playing.js
+++ js/now_playing.js
@@ -35,25 +35,14 @@ function displayMovies(movies) {
   });
 }
 
-// 검색어를 사용하여 영화를 필터링하는 함수
-function searchFilter(data, search) {
-  const searchKeywords = search.toLowerCase().split(' ');
-
-  return data.filter((movie) => {
-    const movieTitle = movie.title.toLowerCase().replace(/\s/g, '');
-
-    return searchKeywords.every((keyword) => movieTitle.includes(keyword));
-  });
-}
-
-// 검색 버튼 클릭 시 호출되는 함수
+// 검색어 입력 시 호출되는 함수
 function search() {
   const searchInput = document.getElementById('input');
-  const searchText = searchInput.value.trim().toLowerCase();
+  const searchText = searchInput.value;
   const moviesContainer = document.getElementById('movies');
 
   // 기존 영화 카드 삭제
-  moviesContainer.innerHTML = '';
+  moviesContainer.innerHTML = ' ';
 
   if (searchText === '') {
     // 검색어가 없는 경우, 모든 영화를 다시 표시
@@ -65,6 +54,15 @@ function search() {
   displayMovies(filteredMovies);
 }
 
+// 검색어를 사용하여 영화를 필터링하는 함수
+function searchFilter(data, search) {
+  return data.filter((movie) => {
+    const movieTitle = movie.title.toLowerCase().replace(/\s/g, '');
+
+    return search.every((keyword) => movieTitle.includes(keyword));
+  });
+}
+
 // 검색어 입력 이벤트를 감지하여 실시간으로 검색 실행
 function handleSearchInput() {
   const searchInput = document.getElementById('input');
@@ -73,18 +71,17 @@ function handleSearchInput() {
   });
 }
 
-// 초기화 및 이벤트 리스너 설정
-function initialize() {
-  fetchMovies();
-  handleSearchInput();
-  document
-    .getElementById('input')
-    .addEventListener('keydown', function (event) {
-      if (event.key === 'Enter') {
-        event.preventDefault(); // 기본 동작 방지
-        search();
-      }
-    });
-}
+// // 초기화 및 이벤트 리스너 설정
+// function initialize() {
+//   fetchMovies();
+//   handleSearchInput();
+//   document
+//     .getElementById('input')
+//     .addEventListener('keydown', function (event) {
+//       if (event.key === 'Enter') {
+//         search();
+//       }
+//     });
+// }
 
-initialize();
+// initialize();
